ed013156c512c284ccbc1b691ca9045cd022dbd2
Madhavan Raja <madhavanraja99@gmail.com>
2021-03-29 17:53:01 +0530

    Did some more things lmao

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from preprocessing import novel_approach, video_to_numpy, video_to_frames
+from preprocessing import novel_approach, video_to_numpy, video_to_frames, motion_detection
 import paths
 
 
 def main():
-    novel_approach.display_preprocessed_video(video_path=paths._DATASET_PATH)
+    # novel_approach.display_preprocessed_video(video_path=paths._DATASET_PATH)
     # video_to_numpy.generate_numpy(paths.source_path, paths.target_path)
     # video_to_frames.generate_frames(source_path=paths.source_path, target_path=paths.target_path)
+    motion_detection.motion_detection(video_path=paths.SOME_OTHER_FILE)
 
 
 if __name__ == '__main__':
--- paths/__init__.py
+++ paths/__init__.py
@@ -2,5 +2,7 @@ import os
 
 _DATASET_PATH = os.path.join('datasets', 'RWF-2000', 'val', 'Fight', 'fight.avi')
 
+SOME_OTHER_FILE = os.path.join('datasets', 'RWF-2000', 'val', 'NonFight', 'rocking_baby.avi')
+
 source_path = os.path.join('datasets', 'RWF-2000')
 target_path = os.path.join('datasets', 'RWF-2000-Target')
